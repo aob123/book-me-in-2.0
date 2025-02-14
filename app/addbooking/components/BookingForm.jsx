f94f67166982f9604a5f0c62b17e9dc715dca900
aob123 <alexobeswick@gmail.com>
2025-02-14 19:57:53 +0100

    Created booking form component

---
+++ app/addbooking/components/BookingForm.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+const BookingForm = () => {
+  return (
+    <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm/6 font-semibold text-white-900"
+          >
+            Name
+          </label>
+          <div className="mt-2.5">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              autoComplete="given-name"
+              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="company"
+            className="block text-sm/6 font-semibold text-white-900"
+          >
+            Company
+          </label>
+          <div className="mt-2.5">
+            <input
+              id="company"
+              name="company"
+              type="text"
+              autoComplete="organization"
+              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm/6 font-semibold text-white-900"
+          >
+            Email
+          </label>
+          <div className="mt-2.5">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="phone-number"
+            className="block text-sm/6 font-semibold text-white-900"
+          >
+            Phone number
+          </label>
+          <div className="mt-2.5">
+            <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+              <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country"
+                  aria-label="Country"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                >
+                  <option>US</option>
+                  <option>CA</option>
+                  <option>EU</option>
+                </select>
+              </div>
+              <input
+                id="phone-number"
+                name="phone-number"
+                type="text"
+                placeholder="123-456-7890"
+                className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="message"
+            className="block text-sm/6 font-semibold text-gray-900"
+          >
+            Message
+          </label>
+          <div className="mt-2.5">
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              defaultValue={""}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-10">
+        <button
+          type="submit"
+          className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Add booking
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default BookingForm;
